0e9d83a3793f90e386864926caa075d10b40867e
Didid <diva.mujaddidi@outlook.com>
2021-07-22 15:36:12 +0700

    updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration; // Memanggil package migration buat menangani migrasi tabel
+use Illuminate\Database\Schema\Blueprint; // Memanggil package blueprint buat menangani blueprint dari tabel
+use Illuminate\Support\Facades\Schema; // Memanggil package schema agar kita dapat membuat scheme dari tabel
 
-class CreateUsersTable extends Migration
+class CreateUsersTable extends Migration // Class untuk membuat Tabel "users"
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up() // fungsi yg dijalankan ketika kita melakukan migrasi
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) { // Untuk membuat schema atau struktur dari tabel user
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -29,8 +29,11 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down() // Buat membatalkan migrasi
     {
         Schema::dropIfExists('users');
     }
 }
+
+
+// Informasi-informasi singkat diatas juga berlaku untuk file migration yg lain
